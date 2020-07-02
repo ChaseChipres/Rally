9e29d0a271ba9ddf00018996f924503fc7938167
Chase Chipres <chasechipres@gmail.com>
2020-07-02 15:53:00 -0700

    Created basic UI for Sign In and Sign Up

---
+++ Rally/SignInViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SignInViewController.swift
+//  Rally
+//
+//  Created by Chase Chipres on 7/2/20.
+//  Copyright © 2020 Chase Chipres. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var signInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        signInButton.layer.cornerRadius = 8
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Rally/SignUpViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SignUpViewController.swift
+//  Rally
+//
+//  Created by Chase Chipres on 7/2/20.
+//  Copyright © 2020 Chase Chipres. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        signUpButton.layer.cornerRadius = 8
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
